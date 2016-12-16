e938648ce39d8e0c4810928a7592870decf663fd
于邦旭 <yubangxu@le.com>
2016-12-16 22:45:43 +0800

    add udpsink

---
+++ src/main/java/org/apache/hadoop/metrics2/sink/udpsink/UdpSink.java
@@ -0,0 +1,112 @@
+package org.apache.hadoop.metrics2.sink.udpsink;
+
+import org.apache.avro.data.Json;
+import org.apache.commons.configuration.SubsetConfiguration;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.classification.InterfaceAudience;
+import org.apache.hadoop.classification.InterfaceStability;
+import org.apache.hadoop.metrics2.AbstractMetric;
+import org.apache.hadoop.metrics2.MetricsRecord;
+import org.apache.hadoop.metrics2.MetricsSink;
+import org.apache.hadoop.metrics2.MetricsTag;
+import org.apache.hadoop.net.DNS;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.*;
+
+/**
+ * Created by yubangxu on 2016/12/16.
+ */
+@InterfaceAudience.Public
+@InterfaceStability.Evolving
+public class UdpSink implements MetricsSink, Closeable {
+
+    private static final Log LOG = LogFactory.getLog(UdpSink.class);
+    private SubsetConfiguration conf ;
+    private String clusterName ;
+    private String udpAddr ;
+    private int udpPort ;
+    private String hostName ;
+
+    public void close() throws IOException {
+
+    }
+
+    public void putMetrics(MetricsRecord metricsRecord) {
+//        for (MetricsTag tags : metricsRecord.tags()){
+//
+//        }
+
+        JSONObject jsonObj = new JSONObject();
+        JSONObject tagObj  = new JSONObject();
+        JSONObject valueObj = new JSONObject();
+        try {
+            jsonObj.put("time",String.valueOf(metricsRecord.timestamp()));
+            jsonObj.put("name",metricsRecord.name());
+            jsonObj.put("hostname",getHostName());
+            for (MetricsTag tag : metricsRecord.tags()) {
+                tagObj.put(tag.name(),tag.value());
+            }
+            for (AbstractMetric metric : metricsRecord.metrics()) {
+                valueObj.put(metric.name(),metric.value());
+            }
+            jsonObj.put("tag",tagObj);
+            jsonObj.put("metrics",valueObj);
+
+            sendUdp(jsonObj.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void flush() {
+
+    }
+
+    public void init(SubsetConfiguration subsetConfiguration) {
+        LOG.info("udpsink");
+        this.conf = subsetConfiguration ;
+
+        if (conf.getString("slave.host.name") != null) {
+            hostName = conf.getString("slave.host.name");
+        } else {
+            try {
+                hostName = DNS.getDefaultHost(
+                        conf.getString("dfs.datanode.dns.interface", "default"),
+                        conf.getString("dfs.datanode.dns.nameserver", "default"));
+            } catch (UnknownHostException uhe) {
+                LOG.error(uhe);
+                hostName = "UNKNOWN.example.com";
+            }
+        }
+        try {
+            parseConfiguration();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void parseConfiguration() {
+        this.clusterName = conf.getString("cluster") ;
+        this.udpAddr = conf.getString("udpaddr") ;
+        this.udpPort = conf.getInt("udpport") ;
+    }
+
+    public String getHostName() {
+        return hostName;
+    }
+
+    private void sendUdp(String msg) throws IOException {
+        DatagramSocket ds  = new DatagramSocket();
+        byte[] buf = msg.getBytes();
+        DatagramPacket dp = new DatagramPacket(buf,buf.length, InetAddress.getByName(this.udpAddr),this.udpPort);//10000为定义的端口
+        ds.send(dp);
+        ds.close();
+    }
+}
