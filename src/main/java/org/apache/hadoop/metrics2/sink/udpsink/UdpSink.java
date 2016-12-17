071dc5688d6503239dffd5428049b4c13e01bf4c
于邦旭 <yubangxu@yubangxudeMacBook-Pro-2.local>
2016-12-17 16:26:30 +0800

    add target

--- src/main/java/org/apache/hadoop/metrics2/sink/udpsink/UdpSink.java
+++ src/main/java/org/apache/hadoop/metrics2/sink/udpsink/UdpSink.java
@@ -21,102 +21,152 @@ import java.net.*;
 @InterfaceStability.Evolving
 public class UdpSink implements MetricsSink, Closeable {
 
-    private static final Log LOG = LogFactory.getLog(UdpSink.class);
-    private SubsetConfiguration conf ;
-    private String clusterName ;
-    private String udpAddr ;
-    private int udpPort ;
-    private String hostName ;
+//    private static final Log LOG = LogFactory.getLog(UdpSink.class);
+//    private SubsetConfiguration conf ;
+//    private String clusterName ;
+//    private String udpAddr ;
+//    private int udpPort ;
+//    private String hostName ;
+//
+//    private static final String FILENAME_KEY = "filename";
+//    private PrintStream writer;
+//    @Override
+//    public void close() throws IOException {
+//        writer.close();
+//    }
+//    @Override
+//    public void putMetrics(MetricsRecord metricsRecord) {
+////        for (MetricsTag tags : metricsRecord.tags()){
+////
+////        }
+//
+////        JSONObject jsonObj = new JSONObject();
+////        JSONObject tagObj  = new JSONObject();
+////        JSONObject valueObj = new JSONObject();
+////        try {
+////            jsonObj.put("time",String.valueOf(metricsRecord.timestamp()));
+////            jsonObj.put("name",metricsRecord.name());
+////            jsonObj.put("hostname",getHostName());
+////            for (MetricsTag tag : metricsRecord.tags()) {
+////                tagObj.put(tag.name(),tag.value());
+////            }
+////            for (AbstractMetric metric : metricsRecord.metrics()) {
+////                valueObj.put(metric.name(),metric.value());
+////            }
+////            jsonObj.put("tag",tagObj);
+////            jsonObj.put("metrics",valueObj);
+////
+////
+////            writer.print(jsonObj.toString());
+////            //sendUdp(jsonObj.toString());
+////        } catch (JSONException e) {
+////            e.printStackTrace();
+////        }
+//        writer.print(this.clusterName);
+//    }
+//    @Override
+//    public void flush() {
+//        writer.flush();
+//    }
+//    @Override
+//    public void init(SubsetConfiguration subsetConfiguration) {
+//        System.out.println("hello sink");
+//        String filename = conf.getString(FILENAME_KEY);
+//        try {
+//            writer = (filename == null) ? System.out
+//                    : new PrintStream(new FileOutputStream(new File(filename)),
+//                    true, "UTF-8");
+//        } catch (Exception e) {
+//            throw new MetricsException("Error creating "+ filename, e);
+//        }
+//
+//        LOG.info("udpsink");
+//        this.conf = subsetConfiguration ;
+//
+//        if (conf.getString("slave.host.name") != null) {
+//            hostName = conf.getString("slave.host.name");
+//        } else {
+//            try {
+//                hostName = DNS.getDefaultHost(
+//                        conf.getString("dfs.datanode.dns.interface", "default"),
+//                        conf.getString("dfs.datanode.dns.nameserver", "default"));
+//            } catch (UnknownHostException uhe) {
+//                LOG.error(uhe);
+//                hostName = "UNKNOWN.example.com";
+//            }
+//        }
+//        try {
+//            parseConfiguration();
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    private void parseConfiguration() {
+//        this.clusterName = conf.getString("cluster") ;
+//        this.udpAddr = conf.getString("udpaddr") ;
+//        this.udpPort = conf.getInt("udpport") ;
+//    }
+//
+//    public String getHostName() {
+//        return this.hostName;
+//    }
+//
+//    private void sendUdp(String msg) throws IOException {
+//        DatagramSocket ds  = new DatagramSocket();
+//        byte[] buf = msg.getBytes();
+//        DatagramPacket dp = new DatagramPacket(buf,buf.length, InetAddress.getByName(this.udpAddr),this.udpPort);//10000为定义的端口
+//        ds.send(dp);
+//        ds.close();
+//    }
 
     private static final String FILENAME_KEY = "filename";
     private PrintStream writer;
-    @Override
-    public void close() throws IOException {
-        writer.close();
-    }
-    @Override
-    public void putMetrics(MetricsRecord metricsRecord) {
-//        for (MetricsTag tags : metricsRecord.tags()){
-//
-//        }
 
-//        JSONObject jsonObj = new JSONObject();
-//        JSONObject tagObj  = new JSONObject();
-//        JSONObject valueObj = new JSONObject();
-//        try {
-//            jsonObj.put("time",String.valueOf(metricsRecord.timestamp()));
-//            jsonObj.put("name",metricsRecord.name());
-//            jsonObj.put("hostname",getHostName());
-//            for (MetricsTag tag : metricsRecord.tags()) {
-//                tagObj.put(tag.name(),tag.value());
-//            }
-//            for (AbstractMetric metric : metricsRecord.metrics()) {
-//                valueObj.put(metric.name(),metric.value());
-//            }
-//            jsonObj.put("tag",tagObj);
-//            jsonObj.put("metrics",valueObj);
-//
-//
-//            writer.print(jsonObj.toString());
-//            //sendUdp(jsonObj.toString());
-//        } catch (JSONException e) {
-//            e.printStackTrace();
-//        }
-        writer.print(this.clusterName);
-    }
     @Override
-    public void flush() {
-        writer.flush();
-    }
-    @Override
-    public void init(SubsetConfiguration subsetConfiguration) {
-        System.out.println("hello sink");
+    public void init(SubsetConfiguration conf) {
         String filename = conf.getString(FILENAME_KEY);
         try {
-            writer = (filename == null) ? System.out
+            writer = filename == null ? System.out
                     : new PrintStream(new FileOutputStream(new File(filename)),
                     true, "UTF-8");
         } catch (Exception e) {
             throw new MetricsException("Error creating "+ filename, e);
         }
+    }
 
-        LOG.info("udpsink");
-        this.conf = subsetConfiguration ;
-
-        if (conf.getString("slave.host.name") != null) {
-            hostName = conf.getString("slave.host.name");
-        } else {
-            try {
-                hostName = DNS.getDefaultHost(
-                        conf.getString("dfs.datanode.dns.interface", "default"),
-                        conf.getString("dfs.datanode.dns.nameserver", "default"));
-            } catch (UnknownHostException uhe) {
-                LOG.error(uhe);
-                hostName = "UNKNOWN.example.com";
-            }
+    @Override
+    public void putMetrics(MetricsRecord record) {
+        writer.print(record.timestamp());
+        writer.print(" ");
+        writer.print(record.context());
+        writer.print(".");
+        writer.print(record.name());
+        String separator = ": ";
+        for (MetricsTag tag : record.tags()) {
+            writer.print(separator);
+            separator = ", ";
+            writer.print(tag.name());
+            writer.print("=");
+            writer.print(tag.value());
         }
-        try {
-            parseConfiguration();
-        }catch (Exception e){
-            e.printStackTrace();
+        for (AbstractMetric metric : record.metrics()) {
+            writer.print(separator);
+            separator = ", ";
+            writer.print(metric.name());
+            writer.print("=");
+            writer.print(metric.value());
         }
+        writer.println();
     }
 
-    private void parseConfiguration() {
-        this.clusterName = conf.getString("cluster") ;
-        this.udpAddr = conf.getString("udpaddr") ;
-        this.udpPort = conf.getInt("udpport") ;
+    @Override
+    public void flush() {
+        writer.flush();
     }
 
-    public String getHostName() {
-        return this.hostName;
-    }
-
-    private void sendUdp(String msg) throws IOException {
-        DatagramSocket ds  = new DatagramSocket();
-        byte[] buf = msg.getBytes();
-        DatagramPacket dp = new DatagramPacket(buf,buf.length, InetAddress.getByName(this.udpAddr),this.udpPort);//10000为定义的端口
-        ds.send(dp);
-        ds.close();
+    @Override
+    public void close() throws IOException {
+        writer.close();
     }
 }
